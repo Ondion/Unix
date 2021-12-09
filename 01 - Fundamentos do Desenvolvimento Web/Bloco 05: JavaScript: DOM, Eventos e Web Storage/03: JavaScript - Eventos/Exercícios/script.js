5be7d5d5c0a2f857da84a536bc404b3bfa8b1bb9
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-09 20:04:46 -0300

    update

---
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/03: JavaScript - Eventos/Exercícios/script.js
@@ -0,0 +1,14 @@
+// function print(recado){
+//     console.log("function")
+// }
+// // func = print;
+
+
+// window.onload = func;
+
+
+let section = document.querySelector(".square-section");
+
+section.addEventListener('mouseover', function(){
+    
+});
