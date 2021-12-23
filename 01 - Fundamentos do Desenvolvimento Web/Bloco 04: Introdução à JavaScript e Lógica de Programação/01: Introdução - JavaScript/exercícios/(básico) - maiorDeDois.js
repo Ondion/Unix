0c852751e1d73dc60109d0dc52041fe59f21fb01
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-23 00:50:15 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

---
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - maiorDeDois.js
@@ -0,0 +1,10 @@
+// Programa que retorna o maior de dois números.
+
+let a = 4;
+let b = 5;
+
+if (a > b) {
+    console.log("valor a é o maior: " + a);
+} else {
+    console.log("valor b é o maior: " + b);
+}
---
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - maiorDeTrês.js
@@ -0,0 +1,6 @@
+// Programa que retorna o maior entre três números.
+
+let a = 1;
+let b = 2;
+let c = 3;
+
