965daa53825e67e31156f31a46db919c7776de02
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-23 02:02:58 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

--- 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - converteNotas.js
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - converteNotas.js
@@ -6,5 +6,16 @@ if (nota >= 90) {
     console.log("A");
 } else if (nota >= 80 && nota <= 89) {
     console.log("B");
-} else if (nota >= 70 && nota <= 89) {
-    console.log("B");
+} else if (nota >= 70 && nota <= 79) {
+    console.log("C");
+} else if (nota >= 60 && nota <= 69) {
+    console.log("D");
+} else if (nota >= 50 && nota <= 59) {
+    console.log("E");
+} else if (nota < 50 && nota >= 0) {
+    console.log("F");
+} else if (nota < 0 || nota > 100) {
+    console.log("Nota com valor invalido!")
+} else {
+    console.log("Esse valor é invalido!")
+}
