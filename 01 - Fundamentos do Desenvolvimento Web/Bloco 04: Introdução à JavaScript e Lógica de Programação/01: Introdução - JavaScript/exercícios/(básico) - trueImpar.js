095cc79760205019b60f0cccf0dd3f15b950ff3d
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-23 02:15:05 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

---
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - trueImpar.js
@@ -0,0 +1,11 @@
+// Programa que defina três números em constantes e retorne true se pelo menos uma das três for impar. Caso contrário, ele retorna false.
+
+let a = 2;
+let b = 2;
+let c = 2;
+
+if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1){
+    console.log(true);
+} else {
+    console.log(false)
+}
---
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - truePar.js
@@ -0,0 +1,11 @@
+// Programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+
+let a = 1;
+let b = 1;
+let c = 1;
+
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0){
+    console.log(true);
+} else {
+    console.log(false)
+}
