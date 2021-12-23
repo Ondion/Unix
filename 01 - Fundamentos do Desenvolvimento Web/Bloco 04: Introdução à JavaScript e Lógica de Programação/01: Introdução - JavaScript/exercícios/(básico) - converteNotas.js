0aea920a142d2f2fdc49d1b69bec03e5876f39af
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-23 01:50:51 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

---
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - converteNotas.js
@@ -0,0 +1,10 @@
+//  Programa que converte uma nota porcentagem (de 0 a 100) em conceitos de A a F.
+
+let nota = 75;
+
+if (nota >= 90) {
+    console.log("A");
+} else if (nota >= 80 && nota <= 89) {
+    console.log("B");
+} else if (nota >= 70 && nota <= 89) {
+    console.log("B");
--- 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - xadrezCheck.js
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: Introdução - JavaScript/exercícios/(básico) - xadrezCheck.js
@@ -4,5 +4,11 @@ let peça = "dAmA".toLowerCase();
 
 switch (peça) {
     case "dama":
-        console.log("")
+        console.log("A dama se movimenta em diagonais ou retas qualquer quantidade de casas.");
+        break;
+    case "outros":
+        console.log("desenhar outras peças");
+        break;
+    default:
+        console.log("essa peça não existe no xadrez.");
 }
