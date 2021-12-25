4ae64656018a95752a3e83c5ffbb9b4000ca2337
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-25 17:41:39 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

--- 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: JavaScript - Primeiros passos/exercícios/(básico) - XXX1.js
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: JavaScript - Primeiros passos/exercícios/(básico) - XXX1.js
@@ -1,3 +1,7 @@
+
+
+
+
 // Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
 // Atente que, sobre o custo do produto, incide um imposto de 20%.
 // Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
