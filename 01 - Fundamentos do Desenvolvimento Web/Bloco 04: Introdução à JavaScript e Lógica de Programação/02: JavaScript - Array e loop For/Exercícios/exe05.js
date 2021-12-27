d654e68ea5906e1aeca5b8d95f88dc40271f9f5f
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-27 15:10:15 -0300

    update

---
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/Exercícios/exe05.js
@@ -0,0 +1,7 @@
+// Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (index of names){
+    console.log(index);
+}
