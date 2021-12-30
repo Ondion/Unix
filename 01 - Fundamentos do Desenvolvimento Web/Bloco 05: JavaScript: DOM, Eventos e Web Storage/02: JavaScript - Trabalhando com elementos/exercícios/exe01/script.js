37af3162b17c814d2d266289b6285186ee19985c
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-30 14:51:58 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

--- 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/02: JavaScript - Trabalhando com elementos/exercícios/exe01/script.js
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/02: JavaScript - Trabalhando com elementos/exercícios/exe01/script.js
@@ -1,7 +1,8 @@
-console.log(document.getElementById("elementoOndeVoceEsta"));
-console.log(document.getElementById("elementoOndeVoceEsta").parentNode.style.backgroundColor = "cyan");
-console.log(document.getElementById("primeiroFilhoDoFilho").innerText = "Texto adicionado conforme EXE 3.");
-console.log(document.getElementById("pai").firstElementChild);
-console.log(document.getElementById("elementoOndeVoceEsta").previousElementSibling);
-console.log(document.getElementById("elementoOndeVoceEsta").nextSibling);
-console.log(document.getElementById("elementoOndeVoceEsta").parentElement);
+console.log("EXE: 1", document.getElementById("elementoOndeVoceEsta"));
+console.log("EXE: 2", document.getElementById("elementoOndeVoceEsta").parentNode.style.backgroundColor = "cyan");
+console.log("EXE: 3", document.getElementById("primeiroFilhoDoFilho").innerText = "Texto adicionado conforme EXE 3.");
+console.log("EXE: 4", document.getElementById("pai").firstElementChild);
+console.log("EXE: 5", document.getElementById("elementoOndeVoceEsta").previousElementSibling);
+console.log("EXE: 6", document.getElementById("elementoOndeVoceEsta").nextSibling);
+console.log("EXE: 7", document.getElementById("elementoOndeVoceEsta").parentElement.lastElementChild.previousElementSibling);
+console.log("EXE: 8", document.getElementById("pai").lastElementChild.previousElementSibling)
