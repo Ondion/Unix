10798179d27e1cd5a32124caf11bf77ea6e5d79a
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-30 17:41:55 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

--- 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/02: JavaScript - Trabalhando com elementos/exercícios/exe03/script.js
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/02: JavaScript - Trabalhando com elementos/exercícios/exe03/script.js
@@ -9,5 +9,3 @@ document.getElementById("pai").removeChild(document.getElementById("terceiroFilh
 document.getElementById("pai").removeChild(document.getElementById("quartoEUltimoFilho")),
 
 );
-
-console.log(document.getElementById("pai").innerText = "");
