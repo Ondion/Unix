e8a8b0ec3369f69321d5dc6e66815c22cbf6ca5f
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-30 14:27:23 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

--- 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/02: JavaScript - Trabalhando com elementos/exercícios/exe01/script.js
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/02: JavaScript - Trabalhando com elementos/exercícios/exe01/script.js
@@ -1,1 +1,6 @@
-console.log(window)
+console.log(document.getElementById("elementoOndeVoceEsta"));
+console.log(document.getElementById("elementoOndeVoceEsta").parentNode.style.backgroundColor = "cyan");
+console.log(document.getElementById("primeiroFilhoDoFilho").innerText = "Texto adicionado conforme EXE 3.");
+console.log(document.getElementById("pai").firstElementChild);
+console.log(document.getElementById("elementoOndeVoceEsta").previousElementSibling);
+console.log();
