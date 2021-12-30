8dd99a5b5b23bf7c81df9c3a4c0016e9c8e245d2
Fabio N. Xavier <fabio.xavier@live.com>
2021-12-30 16:53:28 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

--- 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/02: JavaScript - Trabalhando com elementos/exercícios/exe02/script.js
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/02: JavaScript - Trabalhando com elementos/exercícios/exe02/script.js
@@ -1,4 +1,4 @@
 console.log("EXE 1", document.getElementById("pai").appendChild(document.createElement("div")).innerText = "Irmão do #elementoOndeVoceEsta, conforme EXE 1");
 console.log("EXE 2", document.getElementById("elementoOndeVoceEsta").appendChild(document.createElement("span")).innerText = "Filho do elemento 'elementoOndeVoceEsta', conforme exe 2");
 console.log("EXE 3", document.getElementById("primeiroFilhoDoFilho").appendChild(document.createElement("span")).innerText = "Filho do elemento 'primeiroFilhoDoFilho', conforme exe 3");
-console.log("EXE 4", document.getElementsByTagName("span")[0]);
+console.log("EXE 4", document.getElementsByTagName("span")[0].parentNode.parentNode.nextElementSibling);
