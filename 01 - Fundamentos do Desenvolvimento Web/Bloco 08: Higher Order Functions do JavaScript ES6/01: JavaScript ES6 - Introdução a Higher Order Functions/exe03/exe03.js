b2b74cf00420a327f4870fc981e667bdd6e67eac
Fabio N. Xavier <fabio.xavier@live.com>
2022-01-22 22:52:41 -0300

    update

--- 01 - Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/01: JavaScript ES6 - Introdução a Higher Order Functions/exe03/exe03.js
+++ 01 - Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/01: JavaScript ES6 - Introdução a Higher Order Functions/exe03/exe03.js
@@ -1,5 +1,5 @@
 const RIGHT_ANSWERS =   ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'A', 'B'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 const teste = (certo, teste) => {
   let count = 0;
